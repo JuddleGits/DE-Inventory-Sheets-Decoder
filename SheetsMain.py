edb7b7c97505671c78e49cd31a784a11a61a7d00
JuddleGits <35587182+JuddleGits@users.noreply.github.com>
2020-10-29 15:40:00 -0400

    Update SheetsMain.py

--- SheetsMain.py
+++ SheetsMain.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from GSheetsApp import SheetsApp
 
 root = tk.Tk()
-root.geometry("800x600")
+root.geometry("300x200")
+root.title("DE Inventory Decoder")
 app = SheetsApp(master=root)
 app.mainloop()
