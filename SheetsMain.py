6b5c696e870b4527c0a30e039ff0f693db34e995
JuddleGits <35587182+JuddleGits@users.noreply.github.com>
2021-01-11 11:33:33 -0500

    Delete SheetsMain.py

--- SheetsMain.py
+++
@@ -1,9 +0,0 @@
-import os
-import tkinter as tk
-from GSheetsApp import SheetsApp
-
-root = tk.Tk()
-root.geometry("300x200")
-root.title("DE Inventory Decoder")
-app = SheetsApp(master=root)
-app.mainloop()
